5e6ba869179b1fb024f01072d2ab9c37e7e8c9f3
Дмитрий Ч. <dchaminov@ya.ru>
2024-11-23 10:23:07 +0300

    qpsk demodulation

--- qpskDemodulation.m
+++ qpskDemodulation.m
@@ -1,7 +1,10 @@
-function [outputArg1,outputArg2] = qpskDemodulation(inputArg1,inputArg2)
-%QPSKDEMODULATION Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [bits] = qpskDemodulation(complex_amplitudes)
+ % demodulates QPSK complex amplitudes vector to bitstream
+    bits=zeros(1,length(complex_amplitudes)*2);
+    for i=1:length(complex_amplitudes)
+        bits(2*i-1)=(1-sign(real(complex_amplitudes(i))))/2;
+        bits(2*i)=(1-sign(imag(complex_amplitudes(i))))/2;
+    end
+    bits=floor(bits);
 end
 
