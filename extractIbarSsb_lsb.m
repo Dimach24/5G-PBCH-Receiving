947ebb3414f0992694cb487dc1e0c9f1815eacf7
Дмитрий Ч. <dchaminov@ya.ru>
2024-11-25 14:03:01 +0300

    fixed block zero-start indexation

--- extractIbarSsb_lsb.m
+++ extractIbarSsb_lsb.m
@@ -1,10 +1,10 @@
-function IbarSsb = extractIbarSsb_lsb(dmrs, NCellId)
+function ibarSsb = extractIbarSsb_lsb(dmrs, NCellId)
     % finds the block index that corresponds to the reference signal
 
     corr_data=zeros(1,8);
     % correlating signals
-    for i=1:8
-        corr_data(i)=abs(xcorr(generatePbchDmRs(i,NCellId),dmrs,0,"normalized"));
+    for i=0:7
+        corr_data(i+1)=abs(xcorr(generatePbchDmRs(i,NCellId),dmrs,0,"normalized"));
     end
     % maximum likehood 
     [~,blockIndexLsb]=max(corr_data);
