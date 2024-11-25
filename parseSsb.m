b34ff01492cd9d973037cb421f36dd4fd53da1ce
Дмитрий Ч. <dchaminov@ya.ru>
2024-11-25 14:01:47 +0300

    indexation fix

--- parseSsb.m
+++ parseSsb.m
@@ -1,29 +1,22 @@
 function [pbch,dmrs]=parseSsb(rg,symbols_offset,subcarriers_offset,NCellId)
             % extracts pbch bitstream and dmrs complex 
             % amplitudes from the resource grid
-            
             nu=mod(NCellId,4);
 
-            % subcarrier index initialization 
-            d_solid_i=(0:4:236)+nu;
-            p_solid_i=0:1:239;
-            p_solid_i(d_solid_i+1)=[];
-
-            d_splitted_i=[(0:4:44)+nu, (192:4:236)+nu];
-            p_splitted_i=[0:47,192:239];
-            p_splitted_i((0:4:92)+nu+1)=[];
-
-            % extraction from the resource grid 
-            dmrs=[...
-                rg(subcarriers_offset+d_solid_i+1,symbols_offset+1).',...
-                rg(subcarriers_offset+d_splitted_i+1,symbols_offset+2).',...
-                rg(subcarriers_offset+d_solid_i+1,symbols_offset+3).'...
-                ];
-            pbch=[...
-                rg(subcarriers_offset+p_solid_i+1,symbols_offset+1).',...
-                rg(subcarriers_offset+p_splitted_i+1,symbols_offset+2).',...
-                rg(subcarriers_offset+p_solid_i+1,symbols_offset+3).'...
-                ];
+            % indexes of PBCH DM-Rs
+            dmrs13index=(0:4:236)+nu; 
+            dmrs2index=[(0:4:44)+nu, (192:4:236)+nu];
+            % indexes of PBCH DM-RS
+            pbch13index = setdiff(0:239,dmrs13index);
+            pbch2index  = setdiff([0:47 192:239],dmrs2index);
+            
+            rg=rg(subcarriers_offset+1:subcarriers_offset+240 ...
+                ,symbols_offset+2:symbols_offset+4);
+            % 144 elements of dm-rs
+            dmrs=[rg(dmrs13index+1,1); rg(dmrs2index+1,2); rg(dmrs13index+1,3)];
+            % 540 elements of pbch
+            pbch=[rg(pbch13index+1,1); rg(pbch2index+1,2); rg(pbch13index+1,3)];
+            
             % converting into bitstream
             pbch=qpskDemodulation(pbch);
         end
