6ab3416b8e57d21fbcf7fffbb77b301d49c57107
Дмитрий Ч. <dchaminov@ya.ru>
2024-11-25 15:53:16 +0300

    descramblers fixed (copied from scramblers

--- descramblePayload.m
+++ descramblePayload.m
@@ -1,39 +1,25 @@
 function out_seq = descramblePayload(bits,NCellId,Lmax_)
     % THIS PROCEDURE IS EQUAL TO SCRAMBLING PROCEDURE 
     % [deprecated]
-    
-    % descrambling Procedure of revererse scrambling
-    % after CRC detachment [7.1.2, TS 38.212] 
+    % descrambling Procedure of scrambling
+    % before CRC attachment [7.1.2, TS 38.212] 
     arguments
-        bits (1,:)      % input sequence
-        NCellId (1,1)   % cell ID
-        Lmax_ (1,1)     % maximum number of candidate SS/PBCH 
-                        % blocks in half frame [4.1, TS 38.213]
+        bits (1,:) % input sequence (boolean matrix)
+        NCellId (1,1) % cell ID
+        Lmax_ (1,1) % maximum number of candidate SS/PBCH blocks in half frame [4.1, TS 38.213]
     end
     
-    % init
+    %init
     A = length(bits);
     s = zeros(1,A);
     M = A-3 - (Lmax_ == 10) - 2*(Lmax_ == 20) - 3*(Lmax_ == 64);
-    % 3rd & 2nd LSB of SFN are stored in 
-    % bits 6 & 24 of interleaved sequence
-    nu = [bits(1+6) bits(1+24)]; 
-    nu = bin2dec(num2str(nu));       
+    nu = [bits(1+6) bits(1+24)]; % 3rd & 2nd LSB of SFN are stored in 
+    nu = bit2int(nu.',2);       % bits 6 & 24 of interleaved sequence
 
-    % determination of c
-    x1 = zeros(1,2000);
-    x2 = zeros(1,2000);
-    x1(1) = 1;
-    x1(2:31) = 0;
-    x2(1:31) = fliplr(dec2bin(NCellId,31)); % c_init = N_Cell_ID
-    for n = 1:2000
-        x1(n+31) = mod(x1(n+3)+x1(n),2);
-        x2(n+31) = mod(x2(n+3)+x2(n+2)+x2(n+1)+x2(n),2);
-        n1 = 1:160;
-        c(n1) = mod(x1(n1+1600)+x2(n1+1600),2);
-    end
+    %determination of c
+    c = pseudoRandomSequence(NCellId,160);
 
-    % determination of s
+    %determination of s
     i = 0;
     j = 0;
     while i < A
@@ -50,7 +36,7 @@ function out_seq = descramblePayload(bits,NCellId,Lmax_)
         i = i+1;
     end
 
-    % descrambling procedure
+    %scrambling procedure
     out_seq = zeros (1,A);
     for i = 1:A
         out_seq(i) = mod(bits(i)+ s(i),2);
--- descramblePbch.m
+++ descramblePbch.m
@@ -1,56 +1,19 @@
-function [out_seq] = descramblePbch(bits,NCellId, Lmax_, ibarSsb_lsb)
+function [bits] = descramblePbch(bits,NCellId, Lmax_, ibarSsb_lsb)
     % THIS PROCEDURE IS EQUAL TO SCRAMBLING PROCEDURE 
     % [deprecated]
     % DescrambleProcedure of revererse scrambling
     % after demodulation [7.3.3.1, TS 38.211]
     
     arguments
-        bits (1,:) % input sequence (boolean matrix)
+        bits (1,:)          % input sequence (boolean matrix)
         NCellId (1,1)
-        Lmax_ (1,1) % maximum number of candidate SS/PBCH blocks in half frame [5, TS 38.213]
-        ibarSsb_lsb (1,1) % candidate SS/PBCH block index
+        Lmax_ (1,1)         % maximum number of 
+                            % candidate SS/PBCH blocks in 
+                            % half frame [5, TS 38.213]
+        ibarSsb_lsb (1,1)   % candidate SS/PBCH block index
     end
-    
-    % init
-    A = length(bits);
-    s = zeros(1,A);
-    M = A;
-    
-    % determinaton of nu
-    ibarSsb_lsb = fliplr(dec2bin(ibarSsb_lsb,3));
-    if Lmax_ == 4
-        nu = [ibarSsb_lsb(2) ibarSsb_lsb(1)];
-    else
-        nu = [ibarSsb_lsb(3) ibarSsb_lsb(2) ibarSsb_lsb(1)];
-    end
-    nu = bin2dec(num2str(nu));
-    
-    % determination of c
-    x1 = zeros(1,2000);
-    x2 = zeros(1,2000);
-    x1(1) = 1;
-    x1(2:31) = 0;
-    x2(1:31) = fliplr(int2bit(NCellId,31)); %c_init = N_Cell_ID
-    for n = 1:2000
-        x1(n+31) = mod(x1(n+3)+x1(n),2);
-        x2(n+31) = mod(x2(n+3)+x2(n+2)+x2(n+1)+x2(n),2);
-        n1 = 1:160;
-        c(n1) = mod(x1(n1+1600)+x2(n1+1600),2);
-    end
-    
-    % determination of s
-    i = 0;
-    j = 0;
-    while i < A
-        s(1+i) = c(1+mod(j+nu*M,160));
-        j = j+1;
-        i = i+1;
-    end
-    
-    % descrambling procedure
-    out_seq = zeros (1,A);
-    for i = 1:A
-        out_seq(i) = mod(bits(i)+ s(i),2);
-    end
+    M = 864; % length of generated PBCH bit sequence
+    c = pseudoRandomSequence(NCellId,M*Lmax_); % sequence for PBCH scrambling
+    bits(1:M) = mod(bits(1:M)+ c((1:M)+ibarSsb_lsb*M),2); % iSSB = nu
 end
 
